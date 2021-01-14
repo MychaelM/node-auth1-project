d264c0d33e940aaa3761cd2fa559af4b6fca6538
Mychael M <mychaelbmenges@gmail.com>
2021-01-13 17:22:00 -0700

    added users model and router to server

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const usersRouter = require('./users/users-router')
+const db = require('./database/config')
 
 const server = express()
 const port = process.env.PORT || 5000
 
 server.use(express.json())
+
+server.use(usersRouter)
+
+server.use((err, req, res, next) => {
+  console.lof(err)
+
+  res.status(500).json({
+    message: "Something is broken"
+  })
+})
 
 server.listen(port, () => {
   console.log(`server is listening at port ${port}`)
---
+++ users/users-model.js
@@ -0,0 +1,9 @@
+const db = require('../database/config')
+
+function find() {
+  return db("users").select("id", "username")
+}
+
+module.exports = {
+  find,
+}
